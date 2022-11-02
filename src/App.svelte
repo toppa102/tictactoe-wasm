b519413c597a3161188ab7ae1962137f7058a0f2
Topi Kainulainen <topikanulainen@protonmail.com>
2022-11-02 14:02:26 +0200

    Changed to yarn

--- src/App.svelte
+++ src/App.svelte
@@ -1,53 +1,43 @@
 <script lang="ts">
-    import Board from "./Board.svelte";
-    import { options } from "./options-store";
-
-    async function handleMessage(event) {
-        if (event.detail.winner == "player") {
-            alert("You won!");
-        } else if (event.detail.winner == "ai") {
-            alert("You lost!");
-        } else {
-            alert("Draw!");
-        }
-    }
+  import Board from "./Board.svelte";
+  import { options } from "./options-store";
 </script>
 
 <main>
-    <div id="wrapper">
-        Size: <input
-            type="number"
-            bind:value={$options.size}
-            on:change={() =>
-                ($options.win = Math.min(
-                    Math.max($options.win, 1),
-                    $options.size
-                ))}
-            min="1"
-        />
-        Amount for win:
-        <input
-            type="number"
-            bind:value={$options.win}
-            min="1"
-            max={$options.size}
-        />
-        <Board options={$options} on:message={handleMessage} />
-    </div>
+  <div id="wrapper">
+    Size:
+    <input
+      type="number"
+      bind:value={$options.size}
+      on:change={() =>
+        ($options.win = Math.min(Math.max($options.win, 1), $options.size))}
+      min="1"
+      style="width: 40px;"
+    />
+    Amount for win:
+    <input
+      type="number"
+      bind:value={$options.win}
+      min="1"
+      max={$options.size}
+      style="width: 40px;"
+    />
+
+    <Board options={$options} />
+  </div>
 </main>
 
 <style>
-    #wrapper {
-        margin: auto;
-        text-align: center;
-    }
-    :global(body) {
-        font-family: sans-serif;
-        font-size: 1.15em;
-    }
-    input {
-        font-size: inherit;
-        width: 40px;
-        margin-right: 10px;
-    }
+  #wrapper {
+    margin: auto;
+    text-align: center;
+  }
+  :global(body) {
+    font-family: sans-serif;
+    font-size: 1.3em;
+  }
+  input {
+    font-size: inherit;
+    margin-right: 10px;
+  }
 </style>
--- src/Board.svelte
+++ src/Board.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-  import { createEventDispatcher } from "svelte";
-
-  const dispatch = createEventDispatcher();
-
   export let options;
 
   import init, { check_winner, find_best_move } from "tictactoe-rs";
@@ -14,32 +10,33 @@
   $: board = new Uint8Array(new Array(options.size * options.size).fill(0));
   $: depth = 11 - options.size;
 
-  function checkWinner() {
-    let winner = check_winner(board, options.size, options.win);
+  let winner = undefined;
 
-    if (winner == 9223372036854775807n) {
-      dispatch("message", { winner: "ai" });
-      board = new Uint8Array(new Array(options.size * options.size));
+  function checkWinner() {
+    let winner_value = check_winner(board, options.size, options.win);
+
+    if (winner_value == 9223372036854775807n) {
+      winner = "ai";
       return 1;
     }
-    if (winner == -9223372036854775808n) {
-      dispatch("message", { winner: "player" });
-      board = new Uint8Array(new Array(options.size * options.size));
+    if (winner_value == -9223372036854775808n) {
+      winner = "player";
       return 2;
     }
     if (board.every((x) => x != 0)) {
-      dispatch("message", { winner: "draw" });
-      board = new Uint8Array(new Array(options.size * options.size));
+      winner = "draw";
       return 3;
     }
     return 0;
   }
 
   function playTurn(i: number) {
-    board[i] = 2;
-    if (checkWinner() != 0) return;
-    board[find_best_move(board, options.size, options.win, depth)] = 1;
-    checkWinner();
+    if (checkWinner() == 0) {
+      board[i] = 2;
+      if (checkWinner() != 0) return;
+      board[find_best_move(board, options.size, options.win, depth)] = 1;
+      checkWinner();
+    }
   }
 </script>
 
@@ -47,7 +44,7 @@
   {#await wasm}
     <div style="text-align: center; font-size: 3em;">Loading...</div>
   {:then}
-    <div id="board" style="width: {options.size * 60 + options.size * 2}px;">
+    <div id="board" style="width: {options.size * 80 + options.size * 2}px;">
       {#each board as val, i}
         {#if val == 1}
           <div class="cell"><img src={o} alt="O" /></div>
@@ -81,8 +78,8 @@
   }
   .cell {
     flex: 1;
-    min-width: 60px;
-    min-height: 60px;
+    min-width: 80px;
+    min-height: 80px;
     border: 1px solid gray;
   }
 
